d9389a941c2b1ea09c80267fb7472388d8ff7d66
Etienne Martin <git@emartin.ca>
2020-07-19 22:05:16 -0400

    List authors on homepage

--- src/modules/page-home/page-home.component.tsx
+++ src/modules/page-home/page-home.component.tsx
@@ -5,13 +5,19 @@ import { Layout } from "../layout/layout.component";
 import { Heading } from "../../catalog/heading/heading.component";
 import { Article } from "../../services/article/article.entity";
 import { Category } from "../../services/category/category.entity";
+import { Author } from "../../services/author/author.entity";
 
 export interface PageHomeProps {
   articles: Article[];
   categories: Category[];
+  authors: Author[];
 }
 
-export const PageHome: FC<PageHomeProps> = ({ articles, categories }) => {
+export const PageHome: FC<PageHomeProps> = ({
+  articles,
+  categories,
+  authors
+}) => {
   return (
     <>
       <Head>
@@ -24,13 +30,6 @@ export const PageHome: FC<PageHomeProps> = ({ articles, categories }) => {
       </Head>
       <Layout>
         <Heading component="h1">The release</Heading>
-        <Heading>Categories</Heading>
-        {categories.map(({ name, slug }, index) => (
-          <React.Fragment key={index}>
-            <a href={`/category/${slug}`}>{name}</a>
-            <br />
-          </React.Fragment>
-        ))}
         <Heading>Latest Articles</Heading>
         {articles.map(({ title, slug, thumbnail }, index) => (
           <React.Fragment key={index}>
@@ -41,6 +40,20 @@ export const PageHome: FC<PageHomeProps> = ({ articles, categories }) => {
               <br />
               {thumbnail && <img alt="" src={thumbnail} />}
             </a>
+            <br />
+          </React.Fragment>
+        ))}
+        <Heading>Categories</Heading>
+        {categories.map(({ name, slug }, index) => (
+          <React.Fragment key={index}>
+            <a href={`/category/${slug}`}>{name}</a>
+            <br />
+          </React.Fragment>
+        ))}
+        <Heading>Authors</Heading>
+        {authors.map(({ name, slug }, index) => (
+          <React.Fragment key={index}>
+            <a href={`/author/${slug}`}>{name}</a>
             <br />
           </React.Fragment>
         ))}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,18 @@ import {
 } from "../modules/page-home/page-home.component";
 import { getArticles } from "../services/article/article.service";
 import { getCategories } from "../services/category/category.service";
+import { getAuthors } from "../services/author/author.service";
 
 export const getStaticProps: GetStaticProps<PageHomeProps> = async () => {
   const articles = await getArticles();
   const categories = await getCategories();
+  const authors = await getAuthors();
 
   return {
     props: {
       articles,
-      categories
+      categories,
+      authors
     }
   };
 };
