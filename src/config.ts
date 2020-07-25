7b8f9b4c8384746c8aa91ba7bc720d717e448d9c
Etienne Martin <git@emartin.ca>
2020-07-25 13:59:58 -0400

    twitter meta tags

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const SITE_NAME = "The Release";
+export const TWITTER_HANDLE = "@TheReleaseNews";
 export const ORIGIN = process.env.ORIGIN || "http://localhost:3000";
--- src/modules/page-article/page-article.component.tsx
+++ src/modules/page-article/page-article.component.tsx
@@ -5,7 +5,7 @@ import { Layout } from "../layout/layout.component";
 import { Markdown } from "../../catalog/markdown/markdown.component";
 import { ArticleMetadata } from "./article-metadata/article-metadata.component";
 import { Article } from "../../services/article/article.entity";
-import { SITE_NAME } from "../../config";
+import { ORIGIN, SITE_NAME, TWITTER_HANDLE } from "../../config";
 
 export interface PageArticleProps {
   article: Article;
@@ -34,13 +34,21 @@ export const PagePost: FC<PageArticleProps> = ({
         <meta name="author" content={author.name} />
         <meta name="description" content={description} />
         <meta name="content-type" content="article" />
+
         <meta property="og:image" content={coverImageUrl} />
         <meta property="og:site_name" content={SITE_NAME} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:type" content="article" />
-        {/* TODO: store canonical url in article entity */}
         <meta property="og:url" content={url} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:creator" content={TWITTER_HANDLE} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:domain" content={ORIGIN} />
+        <meta property="twitter:image:src" content={coverImageUrl} />
+        <meta property="twitter:site" content={TWITTER_HANDLE} />
+        <meta property="twitter:title" content={title} />
       </Head>
       <Layout>
         <ArticleMetadata
