707a13ffdd09918c52054e3c316b6d7ea1e58ac0
Etienne Martin <git@emartin.ca>
2020-09-09 23:05:03 +0300

    Social Icons

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,11 @@ export const SITE_NAME = "The Release";
 export const SITE_DESCRIPTION =
   "Your Daily Dose of News About The Tech Industry";
 export const SLOGAN = "Tech's Single Source of Truth";
-export const TWITTER_HANDLE = "@TheReleaseNews";
 export const ORIGIN = process.env.ORIGIN || "http://localhost:3000";
 export const DEFAULT_SOCIAL_IMAGE = `${ORIGIN}/social-image.png`;
 export const DEFAULT_SOCIAL_IMAGE_ALT = `The Release's Logo`;
 export const ITEMS_PER_PAGE = 2;
 export const HOMEPAGE_MAX_ITEMS = 20;
+export const TWITTER_HANDLE = "@TheReleaseNews";
+export const TWITTER_URL = "https://twitter.com/TheReleaseNews";
+export const GITHUB_URL = "https://github.com/the-release/";
--- src/modules/main-menu/main-menu.component.tsx
+++ src/modules/main-menu/main-menu.component.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+
+import { GITHUB_URL, TWITTER_URL } from "../../config";
 import { CloseMenuIcon } from "./close-menu-icon/close-menu-icon.component";
 import { HamburgerIcon } from "./hamburger-icon/hamburger-icon.component";
+import { TwitterIcon } from "./twitter-icon/twitter-icon.component";
+import { GithubIcon } from "./github-icon/github-icon.component";
 
 const Overlay = styled.label`
   position: fixed;
@@ -36,7 +40,7 @@ const Categories = styled.ul`
   font-size: 18px;
   line-height: 32px;
   font-weight: bold;
-  margin: 30px 40px;
+  margin: 20px 40px;
 `;
 
 const Misc = styled.ul`
@@ -44,7 +48,37 @@ const Misc = styled.ul`
   font-size: 14px;
   line-height: 24px;
   color: rgba(255, 255, 255, 0.6);
-  margin: 30px 40px;
+  margin: 20px 40px;
+`;
+
+const SocialNetworks = styled.ul`
+  list-style: none;
+  margin: 20px 40px;
+  display: flex;
+  align-items: center;
+  font-size: 0;
+
+  a {
+    display: block;
+    opacity: 0.6;
+    transition: opacity 0.25s;
+    margin-right: 10px;
+    position: relative;
+
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      margin: -5px;
+    }
+
+    &:hover {
+      opacity: 1;
+    }
+  }
 `;
 
 const OpenMenuButton = styled.label`
@@ -147,7 +181,7 @@ export const MainMenu = () => {
           </CloseMenuButton>
         </NavHead>
         <Categories>
-          <li>Artificial Intelligence</li>
+          <li>AI</li>
           <li>Politics</li>
           <li>Privacy</li>
           <li>Security</li>
@@ -158,6 +192,18 @@ export const MainMenu = () => {
           <li>About</li>
           <li>Contribute</li>
         </Misc>
+        <SocialNetworks>
+          <li>
+            <a href={TWITTER_URL} target="_blank" rel="noreferrer">
+              <TwitterIcon /> Twitter
+            </a>
+          </li>
+          <li>
+            <a href={GITHUB_URL} target="_blank" rel="noreferrer">
+              <GithubIcon /> GitHub
+            </a>
+          </li>
+        </SocialNetworks>
       </Drawer>
     </form>
   );
